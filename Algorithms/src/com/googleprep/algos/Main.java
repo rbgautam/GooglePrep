6354d35cac391e09252cfc9f6a736b1bdb5da6ab
Rahul Gautam <deltaman2000@gmail.com>
2013-10-19 11:54:26 -0500

    Initial Commit of the Google Prep algos

---
+++ Algorithms/src/com/googleprep/algos/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.googleprep.algos;
+
+/**
+ * @author Deltaman
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
